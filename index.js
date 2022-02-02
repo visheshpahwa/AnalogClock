f5465d27a68a049c1a5733ec31a8bf23d99d54bf
Vishesh Pahwa <73529277+visheshpahwa@users.noreply.github.com>
2022-02-02 13:03:54 +0530

    Analog Clock made with pure JS

---
+++ index.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+    d = new Date();
+    hTime = d.getHours();
+    mTime = d.getMinutes();
+    sTime = d.getSeconds();
+    hRotation = 30 * hTime + mTime / 2;
+    mRotation = 6 * mTime + sTime / 10;
+    sRotation = 6 * sTime;
+    hour.style.transform = `rotate(${hRotation}deg)`
+    minute.style.transform = `rotate(${mRotation}deg)`
+    second.style.transform = `rotate(${sRotation}deg)`
+}, 1000);
